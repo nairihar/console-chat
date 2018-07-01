a4a4cf7f132d8b27f33d290cb70f699b47aeb4f5
Nairi Harutyunyan <nairihar99@gmail.com>
2018-07-01 21:20:07 +0400

    initial commit, welcome message

---
+++ lib/chat.ex
@@ -0,0 +1,18 @@
+defmodule CHAT do
+  @moduledoc """
+  Documentation for CHAT.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> CHAT.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ lib/cli.ex
@@ -0,0 +1,5 @@
+defmodule CHAT.CLI do
+  def main(_args) do
+    IO.puts("Welcome to console based chat.")
+  end
+end
